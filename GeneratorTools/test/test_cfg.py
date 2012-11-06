6623131dad395d7f2365c30fcdd222b71860b522
jhgoh <>
2012-11-06 11:15:36 +0000

    pileup weight, use cff files

--- GeneratorTools/test/test_cfg.py
+++ GeneratorTools/test/test_cfg.py
@@ -12,24 +12,10 @@ process.MessageLogger.cerr.FwkReport.reportEvery = 10000
 
 process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring())
 process.source.fileNames.append("/store/cmst3/user/cmgtools/CMG/TTJets_MassiveBinDECAY_TuneZ2star_8TeV-madgraph-tauola/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM/V5_B/PAT_CMG_V5_10_0/patTuple_0.root")
+#process.source.fileNames.append("file:///tmp/jhgoh/patTuple_0.root")
 
-process.recoToGenJet = cms.EDProducer("RecoToGenJetAssociator",
-    genJets = cms.InputTag("ak5GenJetsNoNu"),
-    recoJets = cms.InputTag("cmgPFJetSelCHS"),
-    cuts = cms.PSet(
-        maxDR = cms.double(0.5),
-    ),
-)
-
-process.genJetToPartons = cms.EDProducer("GenJetPartonAssociator",
-    genJets = cms.InputTag("ak5GenJetsNoNu"),
-    genParticles = cms.InputTag("genParticlesPruned"),
-#    genParticles = cms.InputTag("genParticles"),
-    cuts = cms.PSet(
-        minNConstituent = cms.int32(1),
-        minFracConstituent = cms.double(0.5),
-    ),
-)
+process.load("TopAnalysis.GeneratorTools.genJetAssociation_cff")
+process.load("TopAnalysis.GeneratorTools.lumiWeight_cff")
 
 process.out = cms.OutputModule("PoolOutputModule", 
     fileName = cms.untracked.string("out.root"),
@@ -44,5 +30,6 @@ process.outPath = cms.EndPath(process.out)
 process.p = cms.Path(
     process.recoToGenJet
   + process.genJetToPartons
+  + process.lumiWeight
 )
 
