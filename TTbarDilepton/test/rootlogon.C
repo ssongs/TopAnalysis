050d82262243e3e6bce5f719d88ca50d4c2bba5c
jhgoh <>
2012-11-07 15:31:40 +0000

    FWLite

--- TTbarDilepton/test/rootlogon.C
+++ TTbarDilepton/test/rootlogon.C
@@ -1,6 +1,12 @@
 { 
-  
+  cout << "Loading FWLite" << endl;
+  gSystem->Load("libFWCoreFWLite.so");
+  AutoLibraryLoader::enable();
+  gSystem->Load("libDataFormatsFWLite.so");
+  gROOT->ProcessLine("namespace edm {typedef edm::Wrapper<vector<float> > Wrapper<vector<float, allocator<float> > >;}");
+  gROOT->ProcessLine("namespace edm {typedef edm::Wrapper<vector<double> > Wrapper<vector<double, allocator<double> > >;}");
 
+  cout << "Loading TDR style" << endl;
   TStyle *tdrStyle = new TStyle("Plain","Style for P-TDR");
 // For the canvas:
   tdrStyle->SetCanvasBorderMode(0);
@@ -145,6 +151,7 @@
   gROOT->ForceStyle();
   tdrStyle->cd();
 
+  cout << "Setup done." << endl;
 }
 
 
