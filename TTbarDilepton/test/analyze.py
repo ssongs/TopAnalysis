f7df425ff3edf0298ae4adc6156f67b262c8f114
jhgoh <>
2012-12-21 06:55:58 +0000

    ntuple analyzer update, KoPFA compatibility

--- TTbarDilepton/test/analyze.py
+++ TTbarDilepton/test/analyze.py
@@ -17,7 +17,7 @@ samples = {}
 samples["mm"] = [
     "Run2012A-DoubleMu", "Run2012B-DoubleMu", "Run2012C-DoubleMu",
     "Summer12-ZJets", "Summer12-ZJets10To50", "Summer12-WJets",
-    "Summer12-TTJets",
+    "Summer12-TTJets", "Summer12-TTbarTuneZ2",
     "Summer12-SingleToptW", "Summer12-SingleTopBartW",
     "Summer12-WW", "Summer12-WZ", "Summer12-ZZ",
 ]
@@ -25,7 +25,7 @@ samples["mm"] = [
 samples["ee"] = [
     "Run2012A-DoubleElectron", "Run2012B-DoubleElectron", "Run2012C-DoubleElectron",
     "Summer12-ZJets", "Summer12-ZJets10To50", "Summer12-WJets",
-    "Summer12-TTJets",
+    "Summer12-TTJets", "Summer12-TTbarTuneZ2",
     "Summer12-SingleToptW", "Summer12-SingleTopBartW",
     "Summer12-WW", "Summer12-WZ", "Summer12-ZZ",
 ]
@@ -33,7 +33,7 @@ samples["ee"] = [
 samples["me"] = [
     "Run2012A-MuEG", "Run2012B-MuEG", "Run2012C-MuEG",
     "Summer12-ZJets", "Summer12-ZJets10To50", "Summer12-WJets",
-    "Summer12-TTJets",
+    "Summer12-TTJets", "Summer12-TTbarTuneZ2",
     "Summer12-SingleToptW", "Summer12-SingleTopBartW",
     "Summer12-WW", "Summer12-WZ", "Summer12-ZZ",
 ]
@@ -55,12 +55,14 @@ def runVallot(sample, mode, verbose):
     if 'Run' in sample:
         ana = NtupleAnalyzerLite(inputTreePath, outputFilePath, hNEventPath, 1)
     else:
-        ana = NtupleAnalyzerLite(inputTreePath, outputFilePath, hNEventPath, hNEventPath+":1", weightVar="puweight*bweight30CSVM")
+        precut = ""
+        if sample == "Summer12-TTbarTuneZ2": precut = "ttbarGen.diLeptonic_ == 0"
+        ana = NtupleAnalyzerLite(inputTreePath, outputFilePath, hNEventPath, hNEventPath+":1", weightVar="puweight", precut=precut)
 
     cuts = {
-        "ee":["ZMass > 12 && PairSign < 0", "lep1.relIso03() < 0.15 && lep2.relIso03() < 0.15", "abs(ZMass-91.2) > 15", "MET > 30", "nJet30>=4", "nbjets30_CSVM >= 2"],
-        "mm":["ZMass > 12 && PairSign < 0", "lep1.relIso03() < 0.15 && lep2.relIso03() < 0.15", "abs(ZMass-91.2) > 15", "MET > 30", "nJet30>=4", "nbjets30_CSVM >= 2"],
-        "me":["ZMass > 12 && PairSign < 0", "lep1.relIso03() < 0.15 && lep2.relIso03() < 0.15", "abs(ZMass-91.2) > -1", "MET >  0", "nJet30>=4", "nbjets30_CSVM >= 2"],
+        "ee":["ZMass > 12 && PairSign < 0", "lep1.relIso03() < 0.15 && lep2.relIso03() < 0.15", "abs(ZMass-91.2) > 15", "MET > 30", "nJet30>=2", "nbjets30_CSVM >= 1"],
+        "mm":["ZMass > 12 && PairSign < 0", "lep1.relIso03() < 0.15 && lep2.relIso03() < 0.15", "abs(ZMass-91.2) > 15", "MET > 30", "nJet30>=2", "nbjets30_CSVM >= 1"],
+        "me":["ZMass > 12 && PairSign < 0", "lep1.relIso03() < 0.15 && lep2.relIso03() < 0.15", "abs(ZMass-91.2) > -1", "MET >  0", "nJet30>=2", "nbjets30_CSVM >= 1"],
     }
     cutsQCD = {
         "ee":["ZMass > 12 && PairSign > 0", "lep1.relIso03() > 0.15 && lep2.relIso03() > 0.15", "abs(ZMass-91.2) > 15", "MET > 30", "nbjets30_CSVM > 0"],
@@ -74,25 +76,28 @@ def runVallot(sample, mode, verbose):
     }
 
     hists = [
-      ["nvertex", "ZMass", "nJet30", "MET"],
-      ["nvertex", "ZMass", "nJet30", "MET"],
-      ["nvertex", "ZMass", "nJet30", "MET"],
-      ["nvertex", "ZMass", "nJet30", "MET", "nbjets30_CSVM", "ttbar.M_", "kinttbarM",],
-      ["nvertex", "ZMass", "nJet30", "MET", "nbjets30_CSVM", "ttbar.M_", "kinttbarM",],
-      ["nvertex", "ZMass", "nJet30", "MET", "nbjets30_CSVM", "ttbar.M_", "kinttbarM",],
-      ["nvertex", "ZMass", "nJet30", "MET", "nbjets30_CSVM", "ttbar.M_", "kinttbarM",],
+      ["hNVertex", "hLL_m", "hNJets", "hMET_pt", "hNBJet", "hGenTT_m", "hVsumTT_m", "hKinsTT_m",],
+      ["hNVertex", "hLL_m", "hNJets", "hMET_pt", "hNBJet", "hGenTT_m", "hVsumTT_m", "hKinsTT_m",],
+      ["hNVertex", "hLL_m", "hNJets", "hMET_pt", "hNBJet", "hGenTT_m", "hVsumTT_m", "hKinsTT_m",],
+      ["hNVertex", "hLL_m", "hNJets", "hMET_pt", "hNBJet", "hGenTT_m", "hVsumTT_m", "hKinsTT_m",],
+      ["hNVertex", "hLL_m", "hNJets", "hMET_pt", "hNBJet", "hGenTT_m", "hVsumTT_m", "hKinsTT_m", "hGenTT_m__KinsTT_m", "hGenTT_m__VsumTT_m",],
+      ["hNVertex", "hLL_m", "hNJets", "hMET_pt", "hNBJet", "hGenTT_m", "hVsumTT_m", "hKinsTT_m", "hGenTT_m__KinsTT_m", "hGenTT_m__VsumTT_m"],
+      ["hNVertex", "hLL_m", "hNJets", "hMET_pt", "hNBJet", "hGenTT_m", "hVsumTT_m", "hKinsTT_m", "hGenTT_m__KinsTT_m", "hGenTT_m__VsumTT_m"],
     ]
 
-    ana.addHistogram("ZMass", "hLL_m", "Dilepton mass;Dilepton mass (GeV/c^{2});Events", 500, 0, 500)
-    ana.addHistogram("kinttbarM", "hKinsTT_m", "Kinematic t#bar{t} mass;t#bar{t} mass (GeV/c^{2});Events", 2000, 0, 2000)
-    ana.addHistogram("ttbar.M_", "hVsumTT_m", "Vector sum t#bar{t} mass;t#bar{t} mass (GeV/c^{2});Events", 2000, 0, 2000)
-    ana.addHistogram("nJet30", "hNJets", "Jet multiplicity;Jet multiplicity;Events", 10, 0, 10)
-    ana.addHistogram("MET", "hMET_pt", "Missing transverse momentum;Missing transverse momentum (GeV/c);Events", 500, 0, 500)
-    ana.addHistogram("nvertex", "hNVertex", "Number of primary vertices;Number of vertices;Events", 30, 0, 30)
-    ana.addHistogram("nbjets30_CSVM", "hNBJet", "Number of b tag;Number of b tagged jets;Events", 10, 0, 10)
+    ana.addH1("ZMass", "hLL_m", "Dilepton mass;Dilepton mass (GeV/c^{2});Events", 100, 0, 500)
+    ana.addH1("kinttbarM", "hKinsTT_m", "Kinematic t#bar{t} mass;t#bar{t} mass (GeV/c^{2});Events", 400, 0, 2000)
+    ana.addH1("ttbar.M_", "hVsumTT_m", "Vector sum t#bar{t} mass;t#bar{t} mass (GeV/c^{2});Events", 400, 0, 2000)
+    ana.addH1("nJet30", "hNJets", "Jet multiplicity;Jet multiplicity;Events", 10, 0, 10)
+    ana.addH1("MET", "hMET_pt", "Missing transverse momentum;Missing transverse momentum (GeV/c);Events", 100, 0, 500)
+    ana.addH1("nvertex", "hNVertex", "Number of primary vertices;Number of vertices;Events", 30, 0, 30)
+    ana.addH1("nbjets30_CSVM", "hNBJet", "Number of b tag;Number of b tagged jets;Events", 10, 0, 10)
+    ana.addH1("ttbarGen.mass_", "hGenTT_m", "Generator level t#bar{t} mass;t#bar{t} mass (GeV/c^{2});Events", 400, 0, 2000)
+    ana.addH2("ttbarGen.mass_:kinttbarM", "hGenTT_m__KinsTT_m", "Response matrix;Generator level t#bar{t} mass;t#bar{t} mass (GeV/c^{2});Kinematic solution t#bar{t} mass", 400, 0, 2000, 400, 0, 2000)
+    ana.addH2("ttbarGen.mass_:ttbar.M_", "hGenTT_m__VsumTT_m", "Response matrix;Generator level t#bar{t} mass;t#bar{t} mass (GeV/c^{2});Vector sum t#bar{t} mass", 400, 0, 2000, 400, 0, 2000)
 
     for i in range(len(cuts[mode])):
-        ana.addCategory("Step %d" % i, cuts[mode][i], hists[i], [])
+        ana.addCategory("Step %d" % i, cuts[mode][i], hists[i])
         #ana.addCategory("QCD %d" % i, " && ".join(cutsQCD[mode][:i+1]))
         #ana.addCategory("DY %d" % i, " && ".join(cutsDY[mode][:i+1]))
     #ana.addCategory("Step 7", "", hists[0], ["renewCut"])
