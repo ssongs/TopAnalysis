eb93693f762cb4d27b881e7d1563371de349438f
Junghwan John Goh <jhgoh@cern.ch>
2013-10-17 00:14:50 +0200

    all filters for dilepton skim

--- Configuration/python/commonFilters_cff.py
+++ Configuration/python/commonFilters_cff.py
@@ -19,11 +19,81 @@ goodOfflinePrimaryVertices = cms.EDFilter("PrimaryVertexObjectFilter",
     filter = cms.bool(True),
 )
 
+selectedMuons = cms.EDFilter("CandViewSelector",
+    src = cms.InputTag("muons"),
+    cut = cms.string(
+        "abs(eta) < 2.6 && pt > 17"
+        " && isPFMuon && (isGlobalMuon || isTrackerMuon)"),
+    filter = cms.bool(False),
+)
+
+selectedElectrons = cms.EDFilter("CandViewSelector",
+    src = cms.InputTag("gsfElectrons"),
+    cut = cms.string(
+      "abs(eta) < 2.6 && pt > 17"
+      " && gsfTrack.isNonnull"
+      #" && passConversionVeto "
+      " && gsfTrack.trackerExpectedHitsInner.numberOfHits<=0"),
+    filter = cms.bool(False),
+)
+
+zMuMuCands = cms.EDProducer("CandViewShallowCloneCombiner",
+    decay = cms.string("selectedMuons@+ selectedMuons@-"),
+    cut = cms.string("10 < mass"),
+)
+
+zElElCands = cms.EDProducer("CandViewShallowCloneCombiner",
+    decay = cms.string("selectedElectrons@+ selectedElectrons@-"),
+    cut = cms.string("10 < mass"),
+)
+
+zMuElCands = cms.EDProducer("CandViewShallowCloneCombiner",
+    decay = cms.string("selectedMuons@+ selectedElectrons@-"),
+    cut = cms.string("10 < mass"),
+)
+
+nZMuMuCands = cms.EDFilter("CandViewCountFilter",
+    src = cms.InputTag("zMuMuCands"),
+    minNumber = cms.uint32(1),
+)
+nZElElCands = nZMuMuCands.clone(src = cms.InputTag("zElElCands"))
+nZMuElCands = nZMuMuCands.clone(src = cms.InputTag("zMuElCands"))
+
+selectedJets = cms.EDFilter("CandViewSelector",
+    src = cms.InputTag("ak5PFJets"),
+    cut = cms.string(
+        "abs(eta) < 2.6 && pt > 20"
+        " && numberOfDaughters > 1"
+        " && neutralHadronEnergyFraction < 0.99 && neutralEmEnergyFraction < 0.99"
+        " && (abs(eta) >= 2.4 || chargedEmEnergyFraction < 0.99)"
+        " && (abs(eta) >= 2.4 || chargedHadronEnergyFraction > 0.)"
+        " && (abs(eta) >= 2.4 || chargedMultiplicity > 0)"),
+)
+
+nJetFilter = cms.EDFilter("CandViewCountFilter",
+    src = cms.InputTag("selectedJets"),
+    minNumber = cms.uint32(2),
+)
+
 commonSequenceForData = cms.Sequence(
     goodOfflinePrimaryVertices
   + noscraping
+  + selectedJets * nJetFilter
 )
 
 commonSequenceForMC = cms.Sequence(
     goodOfflinePrimaryVertices
+  + selectedJets * nJetFilter
+)
+
+filterDoubleMuSequence = cms.Sequence(
+    selectedMuons * zMuMuCands * nZMuMuCands
+)
+
+filterDoubleElectronSequence = cms.Sequence(
+    selectedElectrons * zElElCands * nZElElCands
+)
+
+filterMuEGSequence = cms.Sequence(
+    selectedMuons * selectedElectrons * zMuElCands * nZMuElCands
 )
